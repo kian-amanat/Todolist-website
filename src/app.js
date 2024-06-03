a62e9432de978b656c270c70026fdeff6a69aadb
Parham Fallah <parhamf7@gmail.com>
2024-06-04 00:40:36 +0330

    add express and test and todo routes to the express app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 import 'dotenv/config';
+import express from 'express';
+
+// Backend modules
+import {router as todoRouter} from './modules/todos/routes.js';
+
+const app = express();
+const serverPort = 3000;
+
+app.get('/test', (req, res) => {
+  res.json({
+    message: 'Express js app is running'
+  });
+});
+
+// attach routers to express app
+app.use(todoRouter);
+
+
+app.listen(serverPort, () => {
+  console.log(`server is running on port ${serverPort}`);
+});
