319704fbb79ac9ed9c0fcd7adf77cf3be04f7184
Parham Fallah <parhamf7@gmail.com>
2024-06-18 08:02:46 +0330

    add cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 // Backend modules
 import {router as todoRouter} from './modules/todos/routes.js';
@@ -7,6 +8,7 @@ import {router as todoRouter} from './modules/todos/routes.js';
 const app = express();
 const serverPort = 3000;
 
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 
 app.get('/test', (req, res) => {
