573c3bed5c09007738ad55cef2c7a4445dc293be
Parham Fallah <parhamf7@gmail.com>
2024-06-18 08:14:10 +0330

    update envs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { EXPRESS_APP } from "./core/secrets/index.js";
+
 
 // Backend modules
 import {router as todoRouter} from './modules/todos/routes.js';
 
 const app = express();
-const serverPort = 3000;
+const serverPort = EXPRESS_APP.port;
 
 app.use(cors());
 app.use(express.json()); // for parsing application/json
